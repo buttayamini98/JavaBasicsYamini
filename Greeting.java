efb95f1e7e3cfb78c724c3c4a608e3e731b9aa77
Yamini <yamini.butta18@gmail.com>
2025-04-13 16:19:27 +0530

    Greeting commit

---
+++ Greeting.java
@@ -0,0 +1,13 @@
+public class Greeting {
+	public static void main (String[] args){
+	System.out.println("Hello"); //print this line
+	System.out.println("Hello" + "Yamini");
+	System.out.println("Hello Yamini \n Good Evening");
+	System.out.println("hello \n yamini \n Good Evening");
+	System.out.println("Hi \t\t Yamini");
+	System.out.println("Hi \t\n how are you \t\n yamini");
+	System.out.println(" \" hello \" ");
+	System.out.println(" \\ hello \\ "); 
+	System.out.println(" \t \\\\\\     \\\\\\  \n \t    \\ Hi \\    \n \t  \\\\\\     \\\\\\"); 
+	}
+}
